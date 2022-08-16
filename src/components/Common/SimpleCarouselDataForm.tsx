9e39f7dbb7484d8766c95fc6e5d17c7af27f1a7a
rjsgml5698 <40594949+rjsgml5698@users.noreply.github.com>
2022-08-16 12:04:07 +0900

    remove console

--- src/components/Common/SimpleCarouselDataForm.tsx
+++ src/components/Common/SimpleCarouselDataForm.tsx
@@ -14,7 +14,6 @@ const SimpleCarouselDataForm: React.FC<IProps> = ({
   const [carouselIndex, setCarouselIndex] = useState<number>(0);
   const isLastIndex = carouselIndex === items.length - 1;
 
-  console.log("###@#");
   const contentItem = (item: any) => {
     return (
       <ContentWrapper>
